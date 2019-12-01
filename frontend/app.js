ed2909b64ec9e7893b1e6f80494f2637abeb631a
Stefano <stefanonoventa@gmail.com>
2019-12-01 17:10:13 +0400

    title

--- frontend/app.js
+++ frontend/app.js
@@ -14,7 +14,9 @@ class App extends React.Component {
           company="Grazie app"
         />
         <VideoBg />
-        <Footer />
+        <Footer 
+          footerInfo = "Copyright © Grazie App | Spread happiness one tip at a time"
+        />
       </div>
     )
   }
@@ -61,9 +63,13 @@ class LoginButtons extends React.Component {
 class VideoBg extends React.Component {
   render() {
     return (
-
       <header id="home" className="min-vh-100">
-        <div className="overlay"></div>
+        <div className="overlay">
+        <MainTitle 
+          appTitle = "The new, unique and only app that spreads Happiness"
+          mainInfo = "Spread happiness one tip at a time"
+        />
+        </div>
         <video playsInLine="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
           <source src="./assets/moneyBackground.mp4" type="video/mp4" />
         </video>
@@ -72,20 +78,36 @@ class VideoBg extends React.Component {
   }
 }
 
+class MainTitle extends React.Component {
+  render () {
+  const {mainInfo, appTitle} = this.props;
+    return (
+      <div className="container h-100 mt-5">
+      <div className="d-flex h-100 text-center align-items-center">
+        <div className="w-100 text-white">
+          <h1 className="display-3 title-lighter"> {appTitle}</h1>
+          <p className="lead mb-0">{mainInfo}</p>
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
+
 class Footer extends React.Component {
   render() {
+    const {footerInfo} = this.props
     return (
       <footer id="footerpad">
         <div className="container">
-          <div className="row">
+          <div className="row m-5">
             <div className="col-md-6 col-lg-8 mx-auto">
               <ul className="list-inline text-center">
-                <li className="list-inline-item"><a href="#"><span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x"></i><i className="fa fa-facebook fa-stack-1x fa-inverse"></i></span></a></li>
-                <li className="list-inline-item"><a href="#"><span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x"></i><i className="fa fa-twitter fa-stack-1x fa-inverse"></i></span></a></li>
-                <li className="list-inline-item"><a href="#"><span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x"></i><i className="fa fa-instagram fa-stack-1x fa-inverse"></i></span></a></li>
-                <li className="list-inline-item"><a href="#"><span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x"></i><i className="fa fa-pinterest fa-stack-1x fa-inverse"></i></span></a></li>
+                <li className="list-inline-item p-3"><a href="#"><i className="facebook icon"></i></a></li>
+                <li className="list-inline-item p-3"><a href="#"><i className="twitter icon"></i></a></li>
+                <li className="list-inline-item p-3"><a href="#"><i className="instagram icon"></i></a></li>
               </ul>
-              <p className="copyright text-muted text-center">Copyright © Your Company 2018 | Web Design by Designer</p>
+              <p className="copyright text-muted text-center">{footerInfo}</p>
             </div>
           </div>
         </div>
